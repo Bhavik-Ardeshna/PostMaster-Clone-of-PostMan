d0dbe28485bd65da43197533f1e62cd6c62f4f14
Ardeshna Bhavik <58507059+Bhavik-Ardeshna@users.noreply.github.com>
2020-06-27 19:17:17 +0530

    first commit

---
+++ postman.js
@@ -0,0 +1,116 @@
+console.log("Arvik's Postman");
+
+// 1. Utility function to get DOM element from string
+function getElementFromString(string) {
+    let div = document.createElement('div');
+    div.innerHTML = string;
+    return div.firstElementChild;
+}
+
+// Initialize no of parameters
+let addedParamCount = 0;
+
+// Hide the parameters box
+let parametersBox = document.getElementById('parametersBox');
+parametersBox.style.display = "none";
+
+//Shoe the text as per radio button
+let paramsRadio = document.getElementById('paramsRadio');
+paramsRadio.addEventListener('click', ()=> {
+  document.getElementById('parametersBox').style.display = "block";
+  document.getElementById('requestJsonBox').style.display = "none";
+});
+
+let jsonRadio = document.getElementById('jsonRadio');
+jsonRadio.addEventListener('click', ()=> {
+  document.getElementById('parametersBox').style.display = "none";
+  document.getElementById('requestJsonBox').style.display = "block";
+});
+
+let addParam =document.getElementById('addParam');
+addParam.addEventListener("click",()=>{
+  let params = document.getElementById('params');
+  let str =  `<div class="form-row my-3">
+                    <label for="url" class="col-sm-2 col-form-label">Parameter ${addedParamCount+2}</label>
+                    <div class="col-md-4">
+                        <input type="text" class="form-control" id="parameterKey${addedParamCount+2}" placeholder="Enter Parameter ${addedParamCount+2} Key">
+                    </div>
+                    <div class="col-md-4">
+                        <input type="text" class="form-control" id="parameterValue${addedParamCount+2}" placeholder="Enter Parameter ${addedParamCount+2} Value">
+                    </div>
+                    <button class="btn btn-warning removeParam"><strong> - </strong></button>
+                </div>`;
+  let addParamDiv = getElementFromString(str);
+  params.appendChild(addParamDiv);
+  let removeParam = document.getElementsByClassName('removeParam');
+  for(p of removeParam){
+    p.addEventListener('click',(e)=>{
+      e.target.parentElement.remove();
+    })
+  }
+  addedParamCount ++;
+})
+
+
+let submit = document.getElementById('submit');
+submit.addEventListener('click',()=>{
+  //response box show mess please wait
+  document.getElementById('prismtext')
+  let responseJsonBox = document.getElementById('prismtext');
+  responseJsonBox.value = "please wait....."
+
+  let url = document.getElementById('url').value;
+  let requestType = document.querySelector("input[name='requestType']:checked").value;
+  let contentType = document.querySelector("input[name='contentType']:checked").value;
+
+  // if user select the content type params
+  if(contentType=="params"){
+    data={};
+    for(let i=0; i< addedParamCount+1 ; i++){
+        if(document.getElementById('parameterKey'+(i+1)) != undefined){
+          let key = document.getElementById('parameterKey'+(i+1)).value;
+          let value = document.getElementById('parameterValue'+(i+1)).value;
+          data[key] = value;
+        }
+    }
+    data - JSON.stringify(data);
+  }
+
+  else{
+    data = document.getElementById('requestJsonText').value;
+  }
+  console.log(url);
+  console.log(requestType);
+  console.log(contentType);
+  console.log(data);
+
+  if (requestType=='GET'){
+        fetch(url, {
+            method: 'GET',
+        })
+        .then(response=> response.text())
+        .then((text) =>{
+            // document.getElementById('responseJsonText').value = text;
+            document.getElementById('prismtext').innerHTML = text;
+            Prism.highlightAll();
+
+        });
+    }
+
+    else{
+      fetch(url, {
+          method: 'POST',
+          body: data,
+          header:{
+            "Content-type": "application/json; charset=UTF-8"
+          }
+      })
+      .then(response=> response.text())
+      .then((text) =>{
+          // document.getElementById('responseJsonText').value = text;
+          document.getElementById('prismtext').innerHTML = text;
+          Prism.highlightAll();
+      });
+    }
+
+})
